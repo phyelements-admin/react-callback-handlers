1a7ffcbca563e914b4bd6330ebab0053317f071e
Girish <111488101+phyelements-admin@users.noreply.github.com>
2022-11-04 16:22:33 +0530

    search by title is implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const story = [
+    {
+      id:1,
+      title: 'Lion and cat',
+      url: 'https:www.com',
+      author:'Ramu',
+      comments:5,
+    },
+    {
+      id:2,
+      title: 'The Mice',
+      url: 'https:www.com',
+      author:'Praveen',
+      comments:8, 
+    },
+    {
+      id:3,
+      title: 'Tom And jerry',
+      url: 'https:www.com',
+      author:'Naveena',
+      comments:8, 
+    },
+    {
+      id:4,
+      title: 'Fox',
+      url: 'https:www.com',
+      author:'Amir',
+      comments:8, 
+    },
+
+  ];
+  
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const searchedStories = story.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search onSearch={handleSearch} />
+      <List story={searchedStories} />
     </div>
   );
 }
+// Search Component
+const Search = (props)=>{
+  const [SearchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+
+    props.onSearch(event);
+  }
+  return(
+  <div>
+    <label >Search By Title</label>
+    <input id="search" type="text" onChange={handleChange}/>
+    <p>
+      Searching for <strong>{SearchTerm}</strong>
+    </p>
+  </div>
+  );
+}
+
+// List Component
+const List = (props)=>(
+  <ul>
+  {props.story.map((item) => (
+// item id and item object is passing to the Item component using Map function
+    <Item key={item.id} item={item}/> 
+  ))}
+  </ul>
+  
+)
+
+// Item Component
+const Item = (props)=>(
+  <li>
+    <span>
+      <a href={props.item.url}> {props.item.title} </a>
+    </span>
+    <span>{props.item.author} </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.points}</span>
+    
+  </li>
+);
 
 export default App;
